8be5c42e954482e082a4e1badfefa43903587395
Don Cooper <don_cooper@hotmail.com>
2017-09-14 11:28:52 -0400

    Companies showing in inspector, but can't get them into React components yet. Placeholder company names iterating properly and being inserted into the correct divs.

--- app/assets/javascripts/components/company.js.jsx
+++ app/assets/javascripts/components/company.js.jsx
@@ -1,13 +1,16 @@
 var Company = React.createClass({
-    render() {
-        return (
-            <div>
-              <ul>
-                <div id="company-row"><li>Company 1</li></div>
-                <div id="company-row"><li>Company 2</li></div>
-                <div id="company-row"><li>Company 3</li></div>
-              </ul>
-            </div>
-        )
-    }
+
+  render() {
+
+    var companies = ['Hubba', 'Shopify', 'Wealthsimple']
+    var companyList = companies.map(function(name) {
+      return <div id="company-row">{name}</div>;
+    })
+
+    return (
+      <div>
+        {companyList}
+      </div>
+    )
+  }
 });
