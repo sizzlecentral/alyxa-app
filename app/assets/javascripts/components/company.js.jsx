7f968c922a922d12ec9796243ce4c7da17f203aa
Don Cooper <don_cooper@hotmail.com>
2017-09-17 11:06:42 -0400

    Company images and names showing up in the correct divs. Company images and names link to company url. Styling rules written so divs resize properly according to content.

--- app/assets/javascripts/components/company.js.jsx
+++ app/assets/javascripts/components/company.js.jsx
@@ -6,7 +6,16 @@ var Company = React.createClass({
     console.log(companyList[0].company.name)
 
     var listed = companyList.map(function(company, index){
-          return <div id='company-row' key={index}>{companyList[index].company.name}</div>;
+          return <div id='company-row' key={index}>
+            <a href={companyList[index].company.url}>
+              <div id='company-image'>
+                <img src={companyList[index].company.image} alt={companyList[index].company.name} />
+              </div>
+              <div id='company-name'>
+                {companyList[index].company.name}
+              </div>
+            </a>
+          </div>;
         })
 
     return (
