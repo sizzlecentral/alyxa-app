a4f94c706bdaf70b59785207afaed1e59324cdb4
Don Cooper <don_cooper@hotmail.com>
2017-09-17 11:29:37 -0400

    Console log removed, anchor tag now opening in new browser tab.

--- app/assets/javascripts/components/company.js.jsx
+++ app/assets/javascripts/components/company.js.jsx
@@ -3,11 +3,10 @@ var Company = React.createClass({
   render() {
 
     var companyList = this.props.companies
-    console.log(companyList[0].company.name)
 
     var listed = companyList.map(function(company, index){
           return <div id='company-row' key={index}>
-            <a href={companyList[index].company.url}>
+            <a href={companyList[index].company.url} target='_blank'>
               <div id='company-image'>
                 <img src={companyList[index].company.image} alt={companyList[index].company.name} />
               </div>
