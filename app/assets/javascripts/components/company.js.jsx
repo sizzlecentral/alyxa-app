30dde8a9e40c76755b99c814f150d148a286a79d
Don Cooper <don_cooper@hotmail.com>
2017-09-18 13:48:29 -0400

    Set fixed size for company images

--- app/assets/javascripts/components/company.js.jsx
+++ app/assets/javascripts/components/company.js.jsx
@@ -8,7 +8,7 @@ var Company = React.createClass({
           return <div id='company-row' key={index}>
             <a href={companyList[index].company.url} target='_blank'>
               <div id='company-image'>
-                <img src={companyList[index].company.image} alt={companyList[index].company.name} />
+                <img src={companyList[index].company.image} alt={companyList[index].company.name} height='75' width='75' />
               </div>
               <div id='company-name'>
                 {companyList[index].company.name}
