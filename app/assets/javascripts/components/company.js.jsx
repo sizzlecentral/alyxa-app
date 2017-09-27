b0d5fef0fb3c8b99cf20b608053372d7f5b22835
Don Cooper <don_cooper@hotmail.com>
2017-09-26 23:32:25 -0400

    Removed anchor from Company component to test out edit function again. Added onChange to company name div, passing in edit function. Changed text color for company names.

--- app/assets/javascripts/components/company.js.jsx
+++ app/assets/javascripts/components/company.js.jsx
@@ -8,18 +8,42 @@ var Company = React.createClass({
     };
   },
 
+  handleCompanyEdit(e) {
+
+    e.preventDefault();
+    var name = this.state.name.trim();
+    var url = this.state.url.trim();
+    var image = this.state.image.trim();
+    if (!name) {
+      return;
+    }
+
+    this.props.onCompanyEdit({
+      name: name,
+      url: url,
+      image: image
+    });
+
+  },
+
+  setValue(field, event) {
+    var object = {};
+    object[field] = event.target.value;
+    this.setState(object);
+  },
+
   render() {
 
     return (
       <div>
-        <a href={this.state.url} target='_blank'>
+
           <div id='company-image'>
             <img src={this.state.image} alt={this.state.name} height='75' width='75' />
           </div>
-          <div id='company-name'>
+          <div id='company-name' onChange={this.handleCompanyEdit}>
             {this.state.name}
           </div>
-        </a>
+
       </div>
     );
 
