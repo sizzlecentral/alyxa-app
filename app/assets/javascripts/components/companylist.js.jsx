8762fa584cdc35fd3beeab52c5bac3216e28b34a
Don Cooper <don_cooper@hotmail.com>
2017-09-26 23:17:43 -0400

    handleCompanyEdit passed as props from CompanyList to Company components.

--- app/assets/javascripts/components/companylist.js.jsx
+++ app/assets/javascripts/components/companylist.js.jsx
@@ -3,11 +3,13 @@ var CompanyList = React.createClass({
   render() {
 
     var data = this.props.data
+    var handleCompanyEdit = this.props.onCompanyEdit
 
     var companyList = data.map(function(company, index) {
       return (
         <div id='company-row' key={index}>
           <Company
+            onCompanyEdit={handleCompanyEdit}
             key={data[index].company.id}
             name={data[index].company.name}
             url={data[index].company.url}
