9ad7ab50243704e8b1d861b0b81c92a47e6f6653
Don Cooper <don_cooper@hotmail.com>
2017-09-28 10:39:56 -0400

    Editable removed from seed file to make sure editable status is toggling properly.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,33 +2,28 @@ Company.create!(
   name: "Company Co.",
   url: "https://google.ca",
   image: "https://unsplash.it/100",
-  editable: "false"
 )
 
 Company.create!(
   name: "Biz Co.",
   url: "https://google.ca",
   image: "https://unsplash.it/100",
-  editable: "false"
 )
 
 Company.create!(
   name: "Serios Co.",
   url: "https://google.ca",
   image: "https://unsplash.it/100",
-  editable: "false"
 )
 
 Company.create!(
   name: "Cash Money Co.",
   url: "https://google.ca",
   image: "https://unsplash.it/100",
-  editable: "false"
 )
 
 Company.create!(
   name: "Chedda Co.",
   url: "https://google.ca",
   image: "https://unsplash.it/100",
-  editable: "false"
 )
