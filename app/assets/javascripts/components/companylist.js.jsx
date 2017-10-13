344be93a61fb309c41d7f8ed796534b33831b213
Don Cooper <don_cooper@hotmail.com>
2017-10-13 12:23:41 -0400

    State toggling properly and correct cards showing depending on what state is chosen.

--- app/assets/javascripts/components/companylist.js.jsx
+++ app/assets/javascripts/components/companylist.js.jsx
@@ -10,6 +10,7 @@ var CompanyList = React.createClass({
     this.setState({
       show: this.state.show
     });
+    console.log(this.state.show);
   },
 
   handleState(e) {
@@ -21,36 +22,114 @@ var CompanyList = React.createClass({
     var data = this.props.data.reverse()
     var handleCompanyEdit = this.props.onCompanyEdit
     var deleteCompany = this.props.onCompanyDelete;
+    var state = this.state.show
 
     var companyList = data.map(function(company, index) {
 
       var id = data[index].company.id;
 
-      if (data[index].company.archived === 'true') {
-        return (
-          <div id='hidden-div' key={data[index].company.id}>
-          </div>
-        )
-      } else {
-        return (
-          <div id='company-wrapper' key={data[index].company.id}>
-            <Company
-              data={data}
-              onCompanyEdit={handleCompanyEdit}
-              onCompanyDelete={function() {
-                  deleteCompany(id, index)
+      if (state === 'current') {
+
+        if (data[index].company.archived === 'true') {
+          return (
+            <div id='hidden-div' key={data[index].company.id}>
+            </div>
+          )
+        } else {
+          return (
+            <div id='company-wrapper' key={data[index].company.id}>
+              <Company
+                data={data}
+                onCompanyEdit={handleCompanyEdit}
+                onCompanyDelete={function() {
+                    deleteCompany(id, index)
+                  }
                 }
-              }
-              id={data[index].company.id}
-              name={data[index].company.name}
-              url={data[index].company.url}
-              image={data[index].company.image}
-              archived={data[index].company.archived}
-              editable={data[index].company.editable}
-              show={data[index].company.show}
-            />
-          </div>
-        )
+                id={data[index].company.id}
+                name={data[index].company.name}
+                url={data[index].company.url}
+                image={data[index].company.image}
+                archived={data[index].company.archived}
+                editable={data[index].company.editable}
+                show={data[index].company.show}
+              />
+            </div>
+          )
+        }
+
+      } else if (state === 'archived') {
+
+        if (data[index].company.archived === 'true') {
+          return (
+            <div id='company-wrapper' key={data[index].company.id}>
+              <Company
+                data={data}
+                onCompanyEdit={handleCompanyEdit}
+                onCompanyDelete={function() {
+                    deleteCompany(id, index)
+                  }
+                }
+                id={data[index].company.id}
+                name={data[index].company.name}
+                url={data[index].company.url}
+                image={data[index].company.image}
+                archived={data[index].company.archived}
+                editable={data[index].company.editable}
+                show={data[index].company.show}
+              />
+            </div>
+          )
+
+        } else {
+          return (
+            <div id='hidden-div' key={data[index].company.id}>
+            </div>
+          )
+        }
+
+      } else {
+        if (data[index].company.archived === 'true') {
+          return (
+            <div id='company-wrapper' key={data[index].company.id}>
+              <Company
+                data={data}
+                onCompanyEdit={handleCompanyEdit}
+                onCompanyDelete={function() {
+                    deleteCompany(id, index)
+                  }
+                }
+                id={data[index].company.id}
+                name={data[index].company.name}
+                url={data[index].company.url}
+                image={data[index].company.image}
+                archived={data[index].company.archived}
+                editable={data[index].company.editable}
+                show={data[index].company.show}
+              />
+            </div>
+          )
+
+        } else {
+          return (
+            <div id='company-wrapper' key={data[index].company.id}>
+              <Company
+                data={data}
+                onCompanyEdit={handleCompanyEdit}
+                onCompanyDelete={function() {
+                    deleteCompany(id, index)
+                  }
+                }
+                id={data[index].company.id}
+                name={data[index].company.name}
+                url={data[index].company.url}
+                image={data[index].company.image}
+                archived={data[index].company.archived}
+                editable={data[index].company.editable}
+                show={data[index].company.show}
+              />
+            </div>
+          )
+        }
       }
 
     });
