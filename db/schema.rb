513c6e5f2efed7803bfde60e0bc50cf1fe28f936
Don Cooper <don_cooper@hotmail.com>
2017-11-14 12:15:51 -0500

    App name change.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108192857) do
+ActiveRecord::Schema.define(version: 20171114045547) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20171108192857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "archived"
+    t.integer "order"
   end
 
 end
